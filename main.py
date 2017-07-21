f14685b3367d1a35a545189e94187b524d814873
Tomas Borba <tomasborba@gmail.com>
2017-07-21 08:19:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ NUMBER_OF_THREADS = 50
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
-#Cria as threads (maximo NUMBER_OF_THREADS
+#Creates threads (max NUMBER_OF_THREADS
 def create_workers():
     for _ in range(NUMBER_OF_THREADS):
         t = threading.Thread(target=work)
@@ -22,14 +22,14 @@ def create_workers():
         t.start()
 
 
-#Proximo Job da fila
+#Next job in line
 def work():
     while True:
         url = queue.get()
         Spider.crawl_page(threading.current_thread().name, url)
         queue.task_done()
 
-#Cada link na fila eh um novo Job
+#Every link in the queue ia a new job
 def create_jobs():
     for link in file_to_set(QUEUE_FILE):
         queue.put(link)
@@ -37,11 +37,11 @@ def create_jobs():
     crawl()
 
 
-#Se existe itens na fila, eh pesquisado
+#If there still are links left in the queue 
 def crawl():
     queued_links = file_to_set(QUEUE_FILE)
     if len(queued_links) > 0:
-        print("Ainda existem "+ str(len(queued_links)) + ' links na fila')
+        print("There still are"+ str(len(queued_links)) + ' links in the queue.')
         create_jobs()
 
 create_workers()
