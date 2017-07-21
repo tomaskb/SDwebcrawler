1745da993bb4da17104b59e1499611414a4a3a2e
Tomas Borba <tomasborba@gmail.com>
2017-07-21 08:21:15 -0300

    Update spider.py

--- spider.py
+++ spider.py
@@ -35,8 +35,8 @@ class Spider:
     @staticmethod
     def crawl_page(thread_name, page_url):
         if page_url not in Spider.crawled:
-            print(thread_name + ' agora buscando em ' + page_url)
-            print('Na fila: ' + str(len(Spider.queue)) + ' | Já pesquisou:  ' + str(len(Spider.crawled)))
+            print(thread_name + ' now searching ' + page_url)
+            print('In queue: ' + str(len(Spider.queue)) + ' | Searched:  ' + str(len(Spider.crawled)))
             Spider.add_links_to_queue(Spider.gather_links(page_url))
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
